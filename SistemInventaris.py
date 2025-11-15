86f85db5a3ec17f355d01b0795dbb586a3fc258d
AltafRabbani28 <rabbanialtaf08@gmail.com>
2025-11-15 21:22:17 +0700

    Refactor input prompts and improve output messages

--- SistemInventaris.py
+++ SistemInventaris.py
@@ -1,48 +1,55 @@
 obat_list = []
 
-
 def tambah_obat():
-    kode = input("masukan kode obat: ")
-    nama = input("masukan nama obat: ")
-    stok = input("masukan stok obat: ")
+    print("=== Tambah Obat ===")
+    kode = input("Masukan kode obat: ")
+    nama = input("Masukan nama obat: ")
+    stok = input("Masukan stok obat: ")
 
-    obat = {"kode obat: ": kode, "nama obat: ": nama, "stok obat ": stok}
+    obat = {
+        "kode": kode,
+        "nama": nama,
+        "stok": stok
+    }
 
     obat_list.append(obat)
-    print(-"--obat berhasil ditambahkan--")
-
+    print("-- Obat berhasil ditambahkan --")
 
 def daftar_obat():
+    print("=== Daftar Obat ===")
+
     if len(obat_list) == 0:
-        print("--Tidak ada obat yang tersedia--")
+        print("-- Tidak ada obat yang tersedia --")
+    
 
-    for i in len(list_obat):
-        print(o["kode"], "-", o["nama"], "| Stok:", o["stok"])
-        print()
-
+    for i in range(len(obat_list)):
+        print(obat_list[i]["kode"], "-", obat_list[i]["nama"], "| Stok:", obat_list[i]["stok"])
+    print()
 
 def menu():
-    while true:
-        print(
-            """
-===== Sistem Inventaris Obat =====
+    while True:
+        print("""
+===== Sistem Inventaris Obat (Versi Mudah) =====
 1. Tambah Obat
-2. Lihat Obat
+2. Daftar Obat
 3. Update Stok
 4. Hapus Obat
 0. Keluar
-"""
-        )
+""")
 
         pilihan = input("Pilih menu: ")
 
         if pilihan == "1":
             tambah_obat()
         elif pilihan == "2":
-            lihat_obat()
+            daftar_obat()
+        elif pilihan == "3":
+            update_stok
+        elif pilihan == "4":
+            hapus_obat
+        elif pilihan == "0":
             break
         else:
-            print("Pilihan tidak valid!\n")
-
+            print("Pilihan tidak valid!")
 
 menu()
