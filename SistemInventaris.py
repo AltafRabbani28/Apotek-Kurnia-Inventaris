f8a50e8bdc4d822356cad121213343a1d195c5dd
irmanurhayati <irman4970@gmail.com>
2025-12-05 12:43:19 +0700

    Update SistemInventaris.py

--- SistemInventaris.py
+++ SistemInventaris.py
@@ -20,4 +20,55 @@ class Inventory:
         self.hash = {}
         self.pq = []
         self.tree = None
+
+2.     def insert_tree(self, node, key, data):
+        if not node: return Node(key, data)
+        if key < node.key:
+            node.left = self.insert_tree(node.left, key, data)
+        else:
+            node.right = self.insert_tree(node.right, key, data)
+        return node
+
+    def tambah(self, n, h, s, e):
+        if n in self.hash: return False
+        o = Obat(n, h, s, e)
+        self.arr.append(o)
+        self.hash[n] = o
+        self.pq.append((h, o))
+        self.pq.sort()
+        self.tree = self.insert_tree(self.tree, h, o)
+        return True
+
+    def cari(self, n):
+        return self.hash.get(n)
+
+    def hapus(self, n):
+        o = self.cari(n)
+        if not o: return False
+        self.arr.remove(o)
+        del self.hash[n]
+        return True
+
+    def update(self, n, h, s, e):
+        o = self.cari(n)
+        if not o: return False
+        o.harga, o.stok, o.exp = h, s, e
+        return True
+
+    def beli(self, n, j):
+        o = self.cari(n)
+        if not o: return None
+        if o.stok < j: return False
+        if datetime.strptime(o.exp, "%Y-%m-%d") < datetime.today():
+            return "exp"
+        o.stok -= j
+        return True
+
+    def sort_nama(self):
+        a = self.arr[:]
+        for i in range(len(a) - 1):
+            for j in range(len(a) - i - 1):
+                if a[j].nama > a[j + 1].nama:
+                    a[j], a[j + 1] = a[j + 1], a[j]
+        return a
         
